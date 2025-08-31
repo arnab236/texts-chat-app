0d82e9bfa50b087a8593ff27da2ffaac369e3d82
Arnab Sarkar <arnabsarkar236@gmail.com>
2025-08-31 14:09:30 +0530

    fixing problem

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -33,8 +33,8 @@ const Sidebar = () => {
                         <h2 className='font-bold'>Texts</h2>
                     </div>
                     <div className='relative py-2 group'>
-                        <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
-                        <div className={`absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100  ${open ? "block" : "hidden"}`} onClick={() => setOpen(!open)}>
+                        <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' onClick={() => setOpen(!open)}/>
+                        <div className={`absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100  ${open ? "block" : "hidden"}`}>
                             <p onClick={()=> navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
                             <hr className='my-2 border-t border-gray-500'/>
                             <p onClick={() => logout()} className='cursor-pointer text-sm'>Logout</p>
